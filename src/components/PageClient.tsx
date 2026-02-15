b5bbd5e92eee5b9667b249379041650f79a8255e
osherhadad6393-sketch <osherhadad6393@gmail.com>
2026-02-14 21:15:09 -0800

    Make PageClient children prop optional for TS build

--- src/components/PageClient.tsx
+++ src/components/PageClient.tsx
@@ -5,7 +5,13 @@ import { EditorProvider } from "@/editor/EditorProvider";
 import { EditableText } from "@/editor/EditableText";
 import { useAdmin } from "@/hooks/useAdmin";
 
-export default function PageClient({ route, defaultPageId, children }: { route: string; defaultPageId: string; children: React.ReactNode }) {
+interface PageClientProps {
+  route: string;
+  defaultPageId: string;
+  children?: React.ReactNode;
+}
+
+export default function PageClient({ route, defaultPageId, children = null }: PageClientProps) {
   const searchParams = useSearchParams();
   const isAdmin = useAdmin();
 
